19f9f3ad81b4ecc225948179712734b5e5b79223
jackcat13 <christophehenry11@gmail.com>
2023-10-04 11:09:14 +0200

    Implement character animation

--- src/model/character.rs
+++ src/model/character.rs
@@ -7,6 +7,12 @@ use crate::model::skills::Skill;
 use crate::raylib_wrapper::draw_handle::DrawRectangle;
 use crate::raylib_wrapper::wrapper::Window;
 
+pub struct Sprite {
+    pub texture: Texture2D,
+    pub offset: i32,
+    pub row_direction: i32,
+}
+
 pub struct Character {
     pub name: String,
     pub hp: i32,
@@ -16,26 +22,35 @@ pub struct Character {
     pub speed: i32,
     pub equipement: Equipement,
     pub skills: Vec<Skill>,
-    pub sprite: Texture2D,
+    pub sprite: Sprite,
     pub position: Vector2,
     pub size: Vector2,
 }
 
+const SPRITE_COLUMNS: i32 = 9;
+pub const SPRITE_STAND_ROW: i32 = 0;
+pub const SPRITE_DOWN_ROW: i32 = 60;
+pub const SPRITE_DOWN_LEFT: i32 = 120;
+pub const SPRITE_DOWN_RIGHT: i32 = 180;
+pub const SPRITE_DOWN_UP: i32 = 240;
+
 impl Character {
 
-    pub fn print_sprite(&self, draw_handle: &mut RaylibMode2D<RaylibDrawHandle>, frame_rec: DrawRectangle, position: Vector2, color: Color) {
-        draw_sprite(&self.sprite, frame_rec, position, color, draw_handle)
+    pub fn print_sprite(&mut self, draw_handle: &mut RaylibMode2D<RaylibDrawHandle>, frame_rec: DrawRectangle, position: Vector2, color: Color) {
+        if self.sprite.offset > SPRITE_COLUMNS { self.sprite.offset = 0 };
+        draw_sprite(&self.sprite, frame_rec, position, color, draw_handle);
+        self.sprite.offset += 1;
     }
 }
 
-fn draw_sprite(sprite: &Texture2D, frame_rec: DrawRectangle, position: Vector2, color: Color, handler: &mut RaylibMode2D<RaylibDrawHandle>) {
+fn draw_sprite(sprite: &Sprite, frame_rec: DrawRectangle, position: Vector2, color: Color, handler: &mut RaylibMode2D<RaylibDrawHandle>) {
     let frame_rec = Rectangle {
-        x: frame_rec.x,
+        x: frame_rec.x + (frame_rec.width * sprite.offset as f32),
         y: frame_rec.y,
         width: frame_rec.width,
         height: frame_rec.height,
     };
-    handler.draw_texture_rec(sprite, frame_rec, position, color);
+    handler.draw_texture_rec(&sprite.texture, frame_rec, position, color);
 }
 
 pub fn crad(window: Rc<RefCell<Window>>, position: Vector2) -> Character {
@@ -54,7 +69,11 @@ pub fn crad(window: Rc<RefCell<Window>>, position: Vector2) -> Character {
             shoes: None,
         },
         skills: vec![],
-        sprite: window.borrow_mut().load_texture("./static/sprites/crad/crad.png"),
+        sprite: Sprite {
+            texture: window.borrow_mut().load_texture("./static/sprites/crad/crad.png"),
+            offset: 0,
+            row_direction: 0,
+        },
         position: position,
         size: Vector2 {
             x: 50.0,
--- src/screens/game.rs
+++ src/screens/game.rs
@@ -3,7 +3,7 @@ use std::rc::Rc;
 use raylib::color::Color;
 use raylib::prelude::{Camera2D, RaylibDraw, RaylibMode2DExt, Vector2};
 use raylib::prelude::KeyboardKey::{KEY_D, KEY_Q, KEY_S, KEY_Z};
-use crate::model::character::Character;
+use crate::model::character::{Character, SPRITE_DOWN_LEFT, SPRITE_DOWN_RIGHT, SPRITE_DOWN_ROW, SPRITE_DOWN_UP, SPRITE_STAND_ROW};
 use crate::model::game::Game;
 use crate::raylib_wrapper::draw_handle::DrawRectangle;
 use crate::raylib_wrapper::wrapper::Window;
@@ -30,17 +30,22 @@ fn reprocess_coordinates(game: &Rc<RefCell<Game>>, window: Rc<RefCell<Window>>) 
 }
 
 fn check_movement(character: &mut Character, window_borrow: RefMut<Window>) {
+    character.sprite.row_direction = SPRITE_STAND_ROW;
     if window_borrow.rl.is_key_down(KEY_S) {
         character.position.y += character.speed as f32;
+        character.sprite.row_direction = SPRITE_DOWN_ROW;
     }
     if window_borrow.rl.is_key_down(KEY_Z) {
         character.position.y -= character.speed as f32;
+        character.sprite.row_direction = SPRITE_DOWN_UP;
     }
     if window_borrow.rl.is_key_down(KEY_D) {
         character.position.x += character.speed as f32;
+        character.sprite.row_direction = SPRITE_DOWN_RIGHT;
     }
     if window_borrow.rl.is_key_down(KEY_Q) {
         character.position.x -= character.speed as f32;
+        character.sprite.row_direction = SPRITE_DOWN_LEFT;
     }
 }
 
@@ -48,22 +53,21 @@ pub fn draw(window: Rc<RefCell<Window>>, game: &Rc<RefCell<Game>>, camera: &mut 
     let mut window_borrow = window.borrow_mut();
     let mut draw_handle = window_borrow.begin_drawing();
     let mut mode_2d = draw_handle.raylib_draw_handle.begin_mode2D(*camera);
-    let game_borrow = game.borrow();
+    let mut game_borrow = game.borrow_mut();
 
     mode_2d.clear_background(Color::GRAY);
 
-    game_borrow.team.characters.first().iter().for_each(|character| {
-        camera.target = character.position;
-        character.print_sprite(&mut mode_2d,
-                               DrawRectangle{
-                                   x: 0.0,
-                                   y: 0.0,
-                                   width: character.size.x,
-                                   height: character.size.y,
-                               },
-                               character.position,
-                               Color::WHITE);
-    });
+    let character: &mut Character = game_borrow.team.characters.first_mut().unwrap();
+    camera.target = character.position;
+    character.print_sprite(&mut mode_2d,
+                           DrawRectangle{
+                               x: 0.0,
+                               y: character.sprite.row_direction as f32,
+                               width: character.size.x,
+                               height: character.size.y,
+                           },
+                           character.position,
+                           Color::WHITE);
 
     mode_2d.draw_text("test", 20, 20, 12, Color::BLACK);
 }
