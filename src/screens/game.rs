77ee66d2b6057f3c56f0b1ef8b959eb2e09ca587
jackcat13 <christophehenry11@gmail.com>
2023-10-30 11:02:33 +0100

    feat: Implement collisions of mouse click with items

--- src/screens/game.rs
+++ src/screens/game.rs
@@ -1,10 +1,11 @@
 use std::cell::{RefCell, RefMut};
 use std::rc::Rc;
 use raylib::color::Color;
-use raylib::prelude::{Camera2D, RaylibDraw, RaylibMode2DExt, Vector2};
+use raylib::prelude::{Camera2D, MouseButton, RaylibDraw, RaylibMode2DExt, Vector2};
 use raylib::prelude::KeyboardKey::{KEY_D, KEY_DOWN, KEY_Q, KEY_S, KEY_UP, KEY_Z};
 use crate::model::character::{Character, SPRITE_DOWN_LEFT, SPRITE_DOWN_RIGHT, SPRITE_DOWN_ROW, SPRITE_DOWN_UP, SPRITE_STAND_ROW};
 use crate::model::game::Game;
+use crate::model::item::Item;
 use crate::model::world::World;
 use crate::procedural::world::generate_random_world;
 use crate::raylib_wrapper::draw_handle::DrawRectangle;
@@ -12,38 +13,40 @@ use crate::raylib_wrapper::wrapper::Window;
 
 pub fn game(window: Rc<RefCell<Window>>, game: Rc<RefCell<Game>>) {
     let random_world = generate_random_world(&window);
-    let (monitor_width, monitor_height) = &window.borrow_mut().get_size();
+    let mut window_borrow = window.borrow_mut();
+    let (monitor_width, monitor_height) = &window_borrow.get_size();
     let mut camera: Camera2D = Camera2D {
         offset: Vector2 { x: *monitor_width as f32 / 2.0, y: *monitor_height as f32 / 2.0 },
         target: Vector2 { x: 0.0, y: 0.0 },
         rotation: 0.0,
-        zoom: 1.5,
+        zoom: 1.0,
     };
 
-    while !&window.borrow_mut().window_should_close() {
-        reprocess_coordinates(&game, &window);
-        reprocess_zoom(&mut camera, &window);
-        draw(&window, &game, &mut camera, &random_world);
+    while !&window_borrow.window_should_close() {
+        let (monitor_width, monitor_height) = &window_borrow.get_size();
+        camera.offset = Vector2 { x: *monitor_width as f32 / 2.0, y: *monitor_height as f32 / 2.0 };
+        reprocess_coordinates(&game, &window_borrow);
+        reprocess_zoom(&mut camera, &window_borrow);
+        process_actions(&window_borrow, &random_world, &camera);
+        draw(&mut window_borrow, &game, &mut camera, &random_world);
     }
 }
 
-fn reprocess_zoom(camera: &mut Camera2D, window: &Rc<RefCell<Window>>) {
-    let window_borrow = window.borrow_mut();
-    if window_borrow.rl.is_key_down(KEY_UP) {
+fn reprocess_zoom(camera: &mut Camera2D, window: &RefMut<Window>) {
+    if window.rl.is_key_down(KEY_UP) {
         camera.zoom += 0.1;
     }
-    if window_borrow.rl.is_key_down(KEY_DOWN) && camera.zoom > 0.5 {
+    if window.rl.is_key_down(KEY_DOWN) && camera.zoom > 0.5 {
         camera.zoom -= 0.1;
     }
 }
 
-fn reprocess_coordinates(game: &Rc<RefCell<Game>>, window: &Rc<RefCell<Window>>) {
-    let window_borrow = window.borrow_mut();
+fn reprocess_coordinates(game: &Rc<RefCell<Game>>, window: &RefMut<Window>) {
     let mut game_borrow = game.borrow_mut();
-    check_movement(game_borrow.team.characters.first_mut().unwrap(), window_borrow);
+    check_movement(game_borrow.team.characters.first_mut().unwrap(), window);
 }
 
-fn check_movement(character: &mut Character, window_borrow: RefMut<Window>) {
+fn check_movement(character: &mut Character, window_borrow: &RefMut<Window>) {
     character.sprite.row_direction = SPRITE_STAND_ROW;
     if window_borrow.rl.is_key_down(KEY_S) {
         character.position.y += character.speed as f32;
@@ -63,9 +66,27 @@ fn check_movement(character: &mut Character, window_borrow: RefMut<Window>) {
     }
 }
 
-fn draw(window: &Rc<RefCell<Window>>, game: &Rc<RefCell<Game>>, camera: &mut Camera2D, random_world: &World) {
-    let mut window_borrow = window.borrow_mut();
-    let mut draw_handle = window_borrow.begin_drawing();
+fn process_actions(window: &RefMut<Window>, random_world: &World, camera: &Camera2D) {
+    if window.rl.is_mouse_button_pressed(MouseButton::MOUSE_BUTTON_LEFT) {
+        let cursor_position = window.rl.get_screen_to_world2D(window.rl.get_mouse_position(), camera);
+        println!("{}::{}", cursor_position.x, cursor_position.y);
+        random_world.items.iter().for_each(|item| {
+            if is_item_collision(cursor_position.x, cursor_position.y, item) {
+                println!("Cursor : {}::{} /// HIT item : {:?}", cursor_position.x, cursor_position.y, item);
+            }
+        })
+    }
+}
+
+fn is_item_collision(cursor_relative_x: f32, cursor_relative_y: f32, item: &Item) -> bool {
+    cursor_relative_x >= item.position.x
+        && cursor_relative_x <= item.position.x + item.sprite.texture.width as f32
+        && cursor_relative_y >= item.position.y
+        && cursor_relative_y <= item.position.y + item.sprite.texture.height as f32
+}
+
+fn draw(window: &mut RefMut<Window>, game: &Rc<RefCell<Game>>, camera: &mut Camera2D, random_world: &World) {
+    let mut draw_handle = window.begin_drawing();
     let mut mode_2d = draw_handle.raylib_draw_handle.begin_mode2D(*camera);
     let mut game_borrow = game.borrow_mut();
     let character: &mut Character = game_borrow.team.characters.first_mut().unwrap();
@@ -93,4 +114,6 @@ fn draw(window: &Rc<RefCell<Window>>, game: &Rc<RefCell<Game>>, camera: &mut Cam
                            },
                            character.position,
                            Color::WHITE);
+
+    mode_2d.draw_rectangle(0, 0, 50, 50, Color::WHITE);
 }
